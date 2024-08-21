f805891684894e46706782649d34babf919c3518
Evgenii <rasshivalov95@mail.ru>
2024-08-21 18:31:12 +0300

    update gcd

--- src/gcd.js
+++ src/gcd.js
@@ -1,22 +1,19 @@
 import getStart from './index.js';
 
-const descriptionOfTheGame = 'Find the greatest common divisor of given numbers.';
+const gameDescription = 'Find the greatest common divisor of given numbers.';
 
 const generateRound = () => {
   const getRandomInt = () => Math.ceil(Math.random() * 10);
   const number1 = getRandomInt();
   const number2 = getRandomInt();
   const randomNumbers = [number1, number2];
-  const question = randomNumbers.join(' ');
 
   const getGreatestCommonDivisor = (num1, num2) => {
     let remainder;
     if (num1 > num2) {
       while (num2 !== 0) {
         remainder = num2;
-        // eslint-disable-next-line no-param-reassign
         num2 = num1 % num2;
-        // eslint-disable-next-line no-param-reassign
         num1 = remainder;
       }
     }
@@ -27,7 +24,8 @@ const generateRound = () => {
   };
 
   const correctAnswer = getGreatestCommonDivisor(number1, number2);
+  const question = randomNumbers.join(' ');
   return [question, correctAnswer];
 };
 
-export default () => getStart(descriptionOfTheGame, generateRound);
+export default () => getStart(gameDescription, generateRound);
