1f3e843e0f3352b4b99a29a427a7779cf0b49b39
Evgenii <rasshivalov95@mail.ru>
2024-08-21 18:53:45 +0300

    acd3

--- src/gcd.js
+++ src/gcd.js
@@ -6,26 +6,18 @@ const generateRound = () => {
   const getRandomInt = () => Math.ceil(Math.random() * 10);
   const number1 = getRandomInt();
   const number2 = getRandomInt();
-  const randomNumbers = [number1, number2];
 
   const getGreatestCommonDivisor = (num1, num2) => {
-    let remainder;
-    if (num1 > num2) {
-      while (num2 !== 0) {
-        remainder = num2;
-        num2 = num1 % num2;
-        num1 = remainder;
-      }
-      return num1
+    while (num2 !== 0) {
+      const remainder = num1 % num2;
+      num1 = num2;
+      num2 = remainder;
     }
-    if (num1 < num2) {
-      return getGreatestCommonDivisor(num2, num1);
-    }
-    return remainder;
+    return num1;
   };
 
-  const correctAnswer = getGreatestCommonDivisor(number1, number2);
-  const question = randomNumbers.join(' ');
+  const correctAnswer = String(getGreatestCommonDivisor(number1, number2));
+  const question = `${number1} ${number2}`;
   return [question, correctAnswer];
 };
 
